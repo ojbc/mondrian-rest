0e3204a7be56402390b19e9f11f728d73e3c2335
Scott Came <scottcame10@gmail.com>
2018-01-23 12:32:10 -0800

    Add license header

--- src/main/java/org/ojbc/mondrian/rest/Application.java
+++ src/main/java/org/ojbc/mondrian/rest/Application.java
@@ -1,12 +1,40 @@
+/*
+ * Unless explicitly acquired and licensed from Licensor under another license, the contents of
+ * this file are subject to the Reciprocal Public License ("RPL") Version 1.5, or subsequent
+ * versions as allowed by the RPL, and You may not copy or use this file in either source code
+ * or executable form, except in compliance with the terms and conditions of the RPL
+ *
+ * All software distributed under the RPL is provided strictly on an "AS IS" basis, WITHOUT
+ * WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND LICENSOR HEREBY DISCLAIMS ALL SUCH
+ * WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific language
+ * governing rights and limitations under the RPL.
+ *
+ * http://opensource.org/licenses/RPL-1.5
+ *
+ * Copyright 2012-2017 Open Justice Broker Consortium
+ */
 package org.ojbc.mondrian.rest;
 
+import javax.annotation.Resource;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
+	
+	public static final String ROLE_REQUEST_ATTRIBUTE_NAME = "mondrian-role";
+	
+	private final Log log = LogFactory.getLog(Application.class);
 	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -15,6 +43,20 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    
+    @Configuration
+    public class MondrianRestConfiguration extends WebMvcConfigurerAdapter {
+    	
+    	@Resource(name="${mondrianRoleInterceptorBeanName}")
+    	private HandlerInterceptor requestToMondrianRoleInterceptor;
+    	
+		@Override
+		public void addInterceptors(InterceptorRegistry registry) {
+			registry.addInterceptor(requestToMondrianRoleInterceptor).addPathPatterns("/**");
+			log.info("Successfully registered request-role interceptor for Mondrian of type "
+					+ requestToMondrianRoleInterceptor.getClass().getName());
+		}	
     }
 
 }
