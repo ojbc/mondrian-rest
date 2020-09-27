d52243aac5c2131ae06290df60e6cb61740e1131
Scott Came <scottcame10@gmail.com>
2020-09-27 08:47:36 -0700

    Allow DefaultRequestAuthorizer to configure a role

--- src/main/java/org/ojbc/mondrian/rest/DefaultRequestAuthorizer.java
+++ src/main/java/org/ojbc/mondrian/rest/DefaultRequestAuthorizer.java
@@ -16,21 +16,36 @@
  */
 package org.ojbc.mondrian.rest;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
- * RequestAuthorizer that authorizes all requests and assigns all requests a null role.
+ * RequestAuthorizer that authorizes all requests and assigns all requests a pre-defined role, or a null role if none is configured.
  */
 @Component
 public class DefaultRequestAuthorizer implements RequestAuthorizer {
+	
+	private final Log log = LogFactory.getLog(DefaultRequestAuthorizer.class);
+	
+	@Value("${defaultRequestAuthorizerRole:#{null}}")
+	private String defaultRequestAuthorizerRole;
+	
+	@PostConstruct
+	public void init() throws Exception {
+		log.info("DefaultRequestAuthorizer will use role " + defaultRequestAuthorizerRole + " for connections");
+	}
 	
 	@Override
 	public RequestAuthorizationStatus authorizeRequest(HttpServletRequest request, String connectionName) throws Exception {
 		RequestAuthorizationStatus ret = new RequestAuthorizationStatus();
 		ret.authorized = true;
 		ret.token = "[None]";
+		ret.mondrianRole = defaultRequestAuthorizerRole;
 		return ret;
 	}
 
